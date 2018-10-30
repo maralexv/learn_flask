4e05ac2209683382d4d745ac831637a50de58848
Alex <marchenko.alexander@gmail.com>
2018-10-30 07:41:48 +0100

    Update mypage1.py

--- mypage1.py
+++ mypage1.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!<h1>"
+    return "<h1>Hello World!</h1>"
+
+@app.route("/info")
+def info():
+    return "<h2>Here you can learn more about us.</h2>"
+
+@app.route("/user/<name>")
+def user(name):
+    return "<h3>This is the page for {}.</h3>".format(name)
+
+@app.route("/admin/<login>")
+    def admin(login):
+        return "10th letter in the Admin Login: {}".format(name[9])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

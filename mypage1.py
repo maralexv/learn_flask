ae24a623e022f07f4a3a40c099641caf700cb35a
Alex <marchenko.alexander@gmail.com>
2018-10-30 08:34:12 +0100

    Update mypage1.py

--- mypage1.py
+++ mypage1.py
@@ -15,8 +15,20 @@ def user(name):
     return "<h3>This is the page for {}.</h3>".format(name)
 
 @app.route("/admin/<login>")
-    def admin(login):
-        return "10th letter in the Admin Login: {}".format(name[9])
+def admin(login):
+    return "10th letter in the Admin Login: {}".format(name[9])
+
+def puppylatinfunc(name):
+    name = name.lower()
+    if name[-1] == 'y':
+        name = name[:-1]
+        return name.capitalize() + 'iful'
+    else:
+        return name.capitalize() + 'y'
+
+@app.route("/puppy_latin/<name>")
+def puppy_latin(name):
+    return "<h2>Hi {}! your puppylatin name is {}.</h2>".format(name, puppylatinfunc(name))
 
 if __name__ == "__main__":
     app.run(debug=True)

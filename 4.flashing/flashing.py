8b403d1e889fe2a1f64de453a57776a567859e6b
Alex <marchenko.alexander@gmail.com>
2018-11-06 07:12:47 +0100

    05.11.18 evening update

--- 4.flashing/flashing.py
+++ 4.flashing/flashing.py
@@ -3,10 +3,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'lrigq)(*$&(*^lejkfba?<aygbe>'
+app.config['SECRET_KEY'] = 'lrigq)(*$&(*^lejkfba?<aygbe'
 
 class SimpleForm(FlaskForm):
 
+	breed = StringField('WHat breed are you?')
 	submit = SubmitField('Click Me')
 
 
@@ -16,7 +17,8 @@ def index():
 	form = SimpleForm()
 
 	if form.validate_on_submit():
-		flash('You just clicked the button')
+		session['breed'] = form.breed.data
+		flash(f"You just changed your {session['breed']}")
 		return redirect(url_for('index'))
 
 	return render_template('index.html', form = form)
@@ -24,3 +26,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
